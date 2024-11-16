0c0052fbe9b6233d15e85d164b813fa86647d2c0
rileyjl5 <rileyjlegere@gmail.com>
2024-11-16 14:48:24 -0400

    Add Category, Buyer, Transaction and Vendor class

--- fft/src/main/java/Test.java
+++
@@ -1,7 +0,0 @@
-package fft.src.main.java;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ fft/src/main/java/Vendor.java
@@ -0,0 +1,22 @@
+package fft.src.main.java;
+
+import java.util.ArrayList;
+public class Vendor {
+    private final String VENDOR_NAME;
+    private final Category CATEGORY;
+    private ArrayList<Vendor> vendors;
+
+    public Vendor(String nameIn, Category catIn){
+        VENDOR_NAME = nameIn;
+        CATEGORY = catIn;
+        vendors.add(this);
+    }
+
+    public String getVendor(){
+        return VENDOR_NAME;
+    }
+
+    public Category getCat(){
+        return CATEGORY;
+    }
+}
