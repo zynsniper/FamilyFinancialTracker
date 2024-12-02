cb96fdefc4f704dc98d6d8c71f11dd8f598ff648
zynsniper <zynsniper41@gmail.com>
2024-12-02 00:04:29 -0400

    add XLSX reader

--- fft/src/main/java/team14/fft/ReadingExcel.java
+++ fft/src/main/java/team14/fft/ReadingExcel.java
@@ -16,30 +16,31 @@ public class ReadingExcel{
 	        XSSFSheet sheet = workBook.getSheetAt(0);
 	
 	        int rows = sheet.getPhysicalNumberOfRows();
-	        String[][] arr = new String[rows][5];
+	        int cols = 5;
+	        String[][] data = new String[rows][cols];
 	    
 	        for(int r = 0; r <= rows ; r++){
 	            XSSFRow row = sheet.getRow(r);
 	            if (row == null) continue;
 	
-	            arr[r][0] = row.getCell(0).getDateCellValue().toString();
-	            arr[r][1] = row.getCell(1).getStringCellValue();
+	            data[r][0] = row.getCell(0).getDateCellValue().toString();
+	            data[r][1] = row.getCell(1).getStringCellValue();
 	
 	            XSSFCell debitCell = row.getCell(2);
 	            if (debitCell != null) {
-	                arr[r][2] = String.valueOf(debitCell.getNumericCellValue());
+	            	data[r][2] = String.valueOf(debitCell.getNumericCellValue());
 	            }
 	
 	            XSSFCell creditCell = row.getCell(3);
 	            if (creditCell != null) {
-	                arr[r][3] = String.valueOf(creditCell.getNumericCellValue());
+	            	data[r][3] = String.valueOf(creditCell.getNumericCellValue());
 	            }
 	
-	            arr[r][4] = String.valueOf(row.getCell(4).getNumericCellValue());
+	            data[r][4] = String.valueOf(row.getCell(4).getNumericCellValue());
 	            
 	        }
 	        
-	        return arr;
+	        return data;
 	    }
     }
 }
