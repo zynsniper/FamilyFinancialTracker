fb690ae232ed5c4dbfbee9a06ba7f0644fe12cc0
zynsniper <zynsniper41@gmail.com>
2024-12-02 00:02:32 -0400

    Provide InputProcessor to format xlsx Input

---
+++ fft/src/main/java/team14/fft/InputProcessor.java
@@ -0,0 +1,17 @@
+package team14.fft;
+
+public class InputProcessor {
+    public void processData(String[][] data) {
+        for (String[] row : data) {
+            if (row == null) continue;
+
+            String date = row[0];
+            String vendor = row[1];
+            String debit = row[2];
+            String credit = row[3];
+            String balance = row[4];
+            
+            System.out.printf("%-30s %-28s %-8s %-8s %-10s %n", date, vendor, debit, credit, balance);
+        }
+    }
+}
