d299993f7a907f6985dc35f57e3de0a16312d79a
Olivia Darrah <oliviadarrah@gmail.com>
2024-12-03 13:03:42 -0400

    add an observableArrayList of Transactions as a TransactionListModel instance variable, add getter

--- fft/src/main/java/team14/fft/TransactionListModel.java
+++ fft/src/main/java/team14/fft/TransactionListModel.java
@@ -3,20 +3,24 @@ package team14.fft;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
 public class TransactionListModel {
 	private SimpleStringProperty buyerStr;
 	private ObservableList<Buyer> buyers;
+	private ObservableList<TransactionModel> observableTransactionModelList;
 	
-	//Contributing authors: R Legere
-	public TransactionListModel() {
+	//Contributing authors: R Legere, O Darrah
+	public TransactionListModel(Statement transactions) {
 		buyerStr = new SimpleStringProperty();
 		buyers = FXCollections.observableArrayList();
-		
-
+		observableTransactionModelList = FXCollections.observableArrayList();
+		for(Transaction transaction: transactions.getTransactions()){
+			TransactionModel temp = new TransactionModel(transaction);
+			observableTransactionModelList.add(temp);
+		}
 	}
+	
 	//Contributing authors: R Legere
 	public SimpleStringProperty buyerStr() {
 		return buyerStr;
@@ -33,5 +37,10 @@ public class TransactionListModel {
 	//Contributing authors: R Legere
 	public ObservableList<Buyer> getList(){
 		return buyers;
+	}
+	
+	//Contributing authors: O Darrah
+	public ObservableList<TransactionModel> getObservableTransactionModels(){
+		return observableTransactionModelList;
 	}
 }
